6c7dee6290d234fa8574083a2b0c992f7db1d87d
Rob Figueiredo <robfig@yext.com>
2014-07-28 18:13:54 -0400

    glock apply: fetch repos when adding/updating.  friendly output

--- apply.go
+++ apply.go
@@ -21,30 +21,32 @@ func init() {
 	cmdApply.Run = runApply // break init loop
 }
 
+var actionstr = map[action]string{
+	add:    "add   ",
+	update: "update",
+	remove: "remove",
+}
+
 func runApply(cmd *Command, args []string) {
 	var gopath = filepath.SplitList(build.Default.GOPATH)[0]
 	var cmds = buildCommands(readDiffLines(os.Stdin))
 	for _, cmd := range cmds {
+		fmt.Printf("%s %-50.49s %s\n", actionstr[cmd.action], cmd.importPath, cmd.revision)
 		var importDir = path.Join(gopath, "src", cmd.importPath)
 		switch cmd.action {
 		case remove:
-			fmt.Println(cmd.importPath, "is no longer in use.")
+			// do nothing
 		case add, update:
+			// add or update the dependency
+			run("go", "get", "-u", path.Join(cmd.importPath, "..."))
+
+			// update that dependency
 			var repo, err = repoRootForImportPath(cmd.importPath)
 			if err != nil {
 				fmt.Println("error determining repo root for", cmd.importPath, err)
 				continue
 			}
-
-			fmt.Println("download", cmd.importPath)
-			err = repo.vcs.download(importDir)
-			if err != nil {
-				fmt.Println("error downloading", cmd.importPath, "to", importDir, "-", err)
-				continue
-			}
-
-			fmt.Println("update", cmd.importPath, "to", cmd.revision)
-			err = repo.vcs.tagSync(importDir, cmd.revision)
+			err = repo.vcs.run(importDir, repo.vcs.tagSyncCmd, "tag", cmd.revision)
 			if err != nil {
 				fmt.Println("error syncing", cmd.importPath, "to", cmd.revision, "-", err)
 				continue
